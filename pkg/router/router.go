baa6370f36fa5155ed005aa8196c586531bf9ebd
kkor1 <xushihao@canway.net>
2026-08-10 09:32:30 +0800

    fix: 修复关于 MaxAge 接收值类型的错误 (#5)

--- pkg/router/router.go
+++ pkg/router/router.go
@@ -58,7 +58,7 @@ func New(slogger *slog.Logger) *gin.Engine {
 	router := gin.New()
 	_ = router.SetTrustedProxies(nil)
 	store := cookie.NewStore([]byte(config.G.Platform.AppSecret))
-	store.Options(sessions.Options{MaxAge: int(30 * time.Minute)})
+	store.Options(sessions.Options{MaxAge: int(30 * time.Minute.Seconds())})
 	router.Use(sessions.Sessions(fmt.Sprintf("%s-session", config.G.Platform.AppID), store))
 
 	// 服务指标
